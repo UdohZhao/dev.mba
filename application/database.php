af3f3109d339d9d356b7835442f74737be9ff556
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-11-29 09:29:03 +0800

    zhaojian

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '112.74.46.113',
     // 数据库名
-    'database'        => '',
+    'database'        => 'db_mba',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'iego',
     // 密码
-    'password'        => '',
+    'password'        => 'iego',
     // 端口
     'hostport'        => '',
     // 连接dsn
