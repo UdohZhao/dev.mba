297e3bb51c5b4931656335418e412b35c0f29017
liujie <liujie@getcunji.com>
2017-11-30 16:07:47 +0800

    liujie

--- public/static/index/index/js/index.js
+++ public/static/index/index/js/index.js
@@ -1,3 +1,19 @@
 $(function(){
 
+	//点击搜索框 发起ajax请求
+    function searchBtn() {
+        var y = document.getElementById('keywords').val();
+        if (y == null || y == '' || y == '站内搜索') {
+            alert('搜索关键字不能为空');
+            return false;
+        } else if (y.length < 2) {
+            alert('请至少输入2个文字');
+            return false
+        } else if (y.length > 40) {
+            alert('最多可输入40个文字');
+            return false
+        }
+    }
+
+
 })
