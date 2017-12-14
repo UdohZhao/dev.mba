c307c7e55342f3ffc96280bc5edfd17729375723
liujie <liujie@getcunji.com>
2017-12-14 18:19:19 +0800

    liujie

--- application/index/controller/Message.php
+++ application/index/controller/Message.php
@@ -125,12 +125,78 @@ class Message extends Base
      */
     public function message()
     {
+        // Get
+        if ($this->request->isGet())
+        {
+            // 读取Logo
+            $data['ad_logoData'] = db('ad_logo')->where('status',0)->find();
+            // 读取顶部广告
+            $data['ad_topData'] = db('ad_top')->where('status',0)->find();
+            // 读取栏目
+            $data['programaData'] = db('programa')->where('pid',0)->select();
+            // 读取栏目子级
+            if ($data['programaData'])
+            {
+                foreach ($data['programaData'] AS $k => $v)
+                {
+                    $data['programaData'][$k]['programaSonData'] = db('programa')->where('pid',$v['id'])->select();
+                    foreach ($data['programaData'][$k]['programaSonData'] AS $kk => $vv)
+                    {
+                        // 读取院校子级发布的文章
+                        if ($vv['type'] == 1)
+                        {
+                            $data['programaData'][$k]['programaSonData'][$kk]['programa_articleData'] = db('programa_article')->where('status',0)->where('pid',$vv['id'])->order('ctime desc')->limit(7)->select();
+                        }
+                        // 读取考试子级发布的文章
+                        if ($vv['type'] == 2)
+                        {
+                            $data['programaData'][$k]['programaSonData'][$kk]['programa_articleData'] = db('programa_article')->where('status',0)->where('pid',$vv['id'])->order('ctime desc')->limit(7)->select();
+                        }
+                    }
+                }
+            }
+            // 读取banner
+            $data['bannerData'] = db('banner')->where('status',0)->order('ctime desc')->select();
+            // 读取MBA热点关注
+            $data['hotData'] = db('programa_article')->where('status',0)->where('type',3)->order('ctime desc')->limit(6)->select();
+            // 读取招考公告
+            $data['announcementData'] = db('programa_article')->where('status',0)->where('type',4)->order('ctime desc')->limit(4)->select();
+            // 读取MBA活动预告广告
+            $data['ad_foreshowData'] = db('ad_foreshow')->where('status',0)->find();
+            // 读取MBA活动预告
+            $data['foreshowData'] = db('programa_article')->where('status',0)->where('type',5)->order('ctime desc')->limit(6)->select();
+            // 读取招生资讯
+            $data['recruitData'] = db('programa_article')->where('status',0)->where('type',7)->order('ctime desc')->limit(7)->select();
+            // 读取考试资讯
+            $data['examData'] = db('programa_article')->where('status',0)->where('type',8)->order('ctime desc')->limit(5)->select();
+            // 读取院校推荐
+            $data['recommendData'] = db('programa_article')->where('status',0)->where('type',6)->order('ctime desc')->limit(5)->select();
+            // 读取院校推荐广告
+            $data['ad_academyData'] = db('ad_academy')->where('status',0)->find();
+            // 读取资讯热线广告
+            $data['ad_informationData'] = db('ad_information')->where('status',0)->find();
+            // 读取手机二维码广告
+            $data['qr_code_phoneData'] = db('qr_code_phone')->where('status',0)->find();
+            // 读取公众号二维码广告
+            $data['qr_code_publicData'] = db('qr_code_public')->where('status',0)->find();
+            // 读取院校发布上方广告
+            $data['ad_academy_topData'] = db('ad_academy_top')->where('status',0)->order('ctime desc')->select();
+            // 读取考试资讯上方广告
+            $data['ad_exam_topData'] = db('ad_exam_top')->where('status',0)->order('ctime desc')->select();
+            // 读取合作伙伴广告
+            $data['ad_partnerData'] = db('ad_partner')->where('status',0)->order('ctime desc')->select();
+            // 读取友情链接
+            $data['blogrollData'] = db('blogroll')->where('status',0)->order('ctime desc')->select();
+            // 读取底部信息
+            $data['webstatData'] = db('webstat')->find();
         // 读取当前栏目下的文章列表
         $data = db('programa_article')->where('status',0)->where('pid',$this->pid)->order('ctime desc')->paginate(config('paging'));
         // 模板变量赋值
+
         $this->assign('data',$data);
         // 渲染模板输出
         return $this->fetch('message');
+            }
     }
 
 }
